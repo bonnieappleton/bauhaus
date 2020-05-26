1e5b8f536895c4e1ea590dc303ee0580b3afdf9e
Bonnie Appleton <bemappleton@gmail.com>
2020-05-26 18:03:47 +0100

    Add sendgrid to requirements and change email to Andrew's

--- home/views.py
+++ home/views.py
@@ -17,7 +17,7 @@ class Contact(FormView):
         subject = 'Website inquiry - ' + form.cleaned_data['name']
         message = form.cleaned_data['email_content']
         sender = form.cleaned_data['contact_email']
-        recipients = ['bemappleton@gmail.com']
+        recipients = ['andrew@bauhausconsultancy.com']
 
         send_mail(subject, message, sender, recipients)
         return super(Contact, self).form_valid(form)
